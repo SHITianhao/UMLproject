e8353ec68263299c3bac38bf6290533af822a38c
SHITianhao <zjsth92@gmail.com>
2015-01-04 23:25:38 +0100

    finmacro bug

--- src/Source/CommandeMacro/CommandeDefMacro.cpp
+++ src/Source/CommandeMacro/CommandeDefMacro.cpp
@@ -21,7 +21,7 @@ void CommandeDefMacro::executer() {
 	Commande::COMMANDE_INSCRITS[nomMacro] = new CommandeMacro(_invoc);
 	string macros;
 	macros = _invoc->readCommandeFromConsole("Macro Commande", _invoc->getRobot());
-	while(CommandeMacro::END_MACRO.compare(macros) != 0 ) {
+	while("FINMACRO" != macros) {
 		_macros.push_back(Commande::nouvelleCommande(macros,_invoc));
 		macros = _invoc->readCommandeFromConsole("Macro Commande", _invoc->getRobot());
 	}
